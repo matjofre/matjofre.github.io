1594dfc6401e6af70b11fe1ffdd0012729ce63ad
matjofre <122750025+matjofre@users.noreply.github.com>
2023-02-09 16:06:44 -0300

    Add files via upload

---
+++ funciones-cv.js
@@ -0,0 +1,6 @@
+//alert("Hola mundo! Esto es un Proyecto Final - Primeros pasos del desarrollo frontend - Argentina Programa 4.0. - Realizado por Jofré Matías");
+
+  
+function enviarFormulario() {
+  alert("Gracias por ponerse en contacto");
+}
